fff82f7bcf9fb6af76fb93abd3ff8cfc844edef1
colins777 <rumyga@gmail.com>
2020-09-02 23:06:33 +0300

    2.09.2020 - header ready 1920px

---
+++ js/common.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+
+    let questions = [
+        {question : '1 Где вЫ планируете открывать ресторан грузинской кухни?',
+            input: '<input type="text" name="city" placeholder="Напишите страну и город, например: Украина, Киев">',
+            answer : ''
+        },
+
+        {question : '2 Есть ли у вас опыт в бизнесе?',
+            input: '<input type="text" name="city" placeholder="Напишите страну и город, например: Украина, Киев"><div class="buisiness-wrap">' +
+                '<p>Да</p><input type="radio" name="buisiness" value="Да">' +
+                '<p>Нет</p><input type="radio" name="buisiness" value="Нет"></div>',
+            answer : ''
+
+        },
+
+        {question : '3 Какой формат ресторана вам интересен?',
+            input: '                    <div>\n' +
+                '                        <p>от 100 м2 с летней площадкой на теплый период</p>\n' +
+                '                        <input type="radio" name="typesBuisiness" value="от 100 м2 с летней площадкой на теплый период">\n' +
+                '                        <p>от 300 м2 с кальянным залом, караоке ﻿и летней площадкой на теплый период</p>\n' +
+                '                        <input type="radio" name="typesBuisiness" value="от 300 м2 с кальянным залом">\n' +
+                '                        <p>Точка — ﻿еда навынос</p>\n' +
+                '                        <input type="radio" name="typesBuisiness" value="Точка — ﻿еда навынос">\n' +
+                '                    </div>',
+            answer : ''
+
+        },
+
+        {question : '4 Где вЫ планируете открывать ресторан грузинской кухни?',
+            input: '<input type="text" name="city" placeholder="Напишите страну и город, например: Украина, Киев">',
+            answer : ''
+        },
+
+
+    ];
+
+    let quizAsk = $('.question');
+    let answerWrap = $('.answer');
+
+    let currentQuest = 0;
+    let totalQuest = questions.length;
+    let questIndex = 0;
+    let userAnswers = [];
+
+    //console.log(totalQuest);
+
+    function loadQuestion (questIndex) {
+        let q = questions[questIndex];
+        let qQuest = questions[questIndex].question;
+        let input = questions[questIndex].input;
+        quizAsk.html(qQuest);
+        answerWrap.html(input);
+    }
+
+    loadQuestion (questIndex);
+
+    function changeSteps () {
+       let steps = $('.quiz-steps__item');
+
+           if (steps[questIndex].classList.contains('steps--active')) {
+               return false;
+           }
+            else {
+               $(steps[questIndex]).addClass('steps--active');
+       }
+    }
+
+    function loadNextQuestion () {
+        questIndex++;
+        loadQuestion(questIndex);
+    }
+
+    function saveValues () {
+        let form = $('form');
+        form.action = 'https://google.com/search';
+        form.method = 'GET';
+        form.submit();
+    }
+
+    $('.button-next').click(function () {
+        loadNextQuestion (questIndex);
+        changeSteps ();
+       // saveValues ();
+    });
+
+
+//remove symbols
+    $("body").children().each(function() {
+        document.body.innerHTML = document.body.innerHTML.replace(/\u2028/g, ' ');
+    });
+
+
+});
